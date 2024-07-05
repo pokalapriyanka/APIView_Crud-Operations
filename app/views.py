35e75b9dbb5307c5b1cc187ce9044af5a9ce722a
pokalapriyanka <pokalapriyanka123@gmail.com>
2024-07-05 13:48:46 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -9,21 +9,13 @@ from app.serializers import *
 
 
 class ProductCrud(APIView):
-    def get(self,request):
+    def get(self,request,pk):
         LPO=Product.objects.all()
         MSPO=ProductMS(LPO,many=True)
         return Response(MSPO.data)
 
 
-    def post(self,request):
-        '''PMSD=ProductSerializer(data=request.data)
-        if PMSD.is_valid():
-            PMSD.save()
-            return Response({'Suceess':'Product is inserted succesfully'})
-        else:
-            return Response({'failed':'Product is not inserted'})
-
-         def post(self,request):'''
+    def post(self,request,pk):
         rjd=request.data
         PDO=ProductMS(data=rjd)
         if PDO.is_valid():
@@ -31,3 +23,22 @@ class ProductCrud(APIView):
             return Response({'success':'Data is inserted successfully'})
         else:
             return Response({'Failed':'Issues while inserting'})
+
+    def put(self,request,pk):
+        rjd=request.data
+        instance=Product.objects.get(pk=pk)
+        LPO=ProductMS(instance,data=rjd)
+        if LPO.is_valid():
+            LPO.save()
+            return Response({'updated':'Updated success'})
+    def patch(self,request,pk):
+        rjd=request.data
+        instance=Product.objects.get(pk=pk)
+        LPO=ProductMS(instance,data=rjd,partial=True) 
+        if LPO.is_valid():
+            LPO.save()
+            return Response({'partial updated':'Updated success'})
+
+    def delete(self,request,pk):
+        Product.objects.get(pk=pk).delete()
+        return Response({'delete':'Deleted Success'})
--- restproject2/urls.py
+++ restproject2/urls.py
@@ -20,5 +20,5 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ProductCrud/',ProductCrud.as_view(),name='ProductCrud'),
+    path('ProductCrud/<pk>',ProductCrud.as_view(),name='ProductCrud'),
 ]
